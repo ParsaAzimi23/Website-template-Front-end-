749a9c580e7536d93e484d8136fa43e53c16729e
ParsaAzimi23 <pazimi730@gmail.com>
2023-01-09 02:42:52 +0330

    added some new fonts

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from "./Body.module.css"
+
 const Body = () => {
     return (
         <div className={styles.container}>
-            
+            <p className={styles.number1}>01 <span>Get Started<br/>What makes you feel <span className={styles.otherFont}>comfortabel</span> ?</span></p>
+            {/* <p className={styles.number1Text}>What makes you feel comfortabel?</p> */}
         </div>
     );
 };
