9ccef98bded755c71b962f8024511ba8061cdf4a
ParsaAzimi23 <pazimi730@gmail.com>
2023-01-10 15:32:42 +0330

    added some props(image and text)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from "./Body.module.css"
+import Image from "../Images/3.jfif"
 
 const Body = () => {
     return (
-        <div className={styles.container}>
-            <p className={styles.number1}>01 <span>Get Started<br/>What makes you feel <span className={styles.otherFont}>comfortabel</span> ?</span></p>
-            {/* <p className={styles.number1Text}>What makes you feel comfortabel?</p> */}
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <p className={styles.number1}>01 <span>Get Started<br/>What makes you feel <span className={styles.otherFont}>comfortabel</span> ?</span></p>
+                <img src={Image} alt='01'/>
+                <p className={styles.number2}>Lorem, ipsum dolor sit amet conse
+                ctetur adipisicing elit. Unde hic  placeat, culpali ber oani miex p lica boqui,eos n e cessitatibus 
+                reprehenderit aspernatur nihil nam at, quos praesentium.</p>
+            </div>
+            <div className={styles.container2}>
+                <p className={styles.number3}>02</p>
+            </div>
         </div>
     );
 };
